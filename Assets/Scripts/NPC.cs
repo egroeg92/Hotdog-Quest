3869dd6271053383136608c77a433847a138a03e
John Saigle <john.saigle@gmail.com>
2015-03-28 18:21:00 -0400

    Complete steering behaviours for seek and flee

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+	public float MAX_VELOCITY;
+	public Vector3 position;
+    public Vector3 velocity;
+
+	public NPC (Vector3 position) {
+		this.transform.position = position;
+		// steering = new SteeringManager(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public Vector3 getVelocity() {
+		return this.velocity;
+	}
+
+	public float getMaxVelocity() {
+		return this.MAX_VELOCITY;
+	}
+
+	public Vector3 getPosition(){
+		return this.transform.position;
+	}
+
+}
