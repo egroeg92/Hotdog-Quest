ef75099229339d26084e9b3e00c0a208e7cdcff4
John Saigle <john.saigle@gmail.com>
2015-04-01 16:04:30 -0400

    Added prefabs and scripts from gamestructure

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : NPC {
+
+	public PlayerMovement movement;
+
+	public PlayerCharacter(Vector3 position) : base(position) {
+		Debug.Log("Player character created.");
+	}
+
+	// Use this for initialization
+	void Start () {
+		movement = new PlayerMovement(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		velocity = movement.update();
+
+		velocity = velocity.normalized;
+		velocity *= MAX_VELOCITY;
+		velocity = truncate(velocity, MAX_VELOCITY);
+
+		//Update movement
+		transform.Translate(velocity * Time.deltaTime);
+
+	}
+}
