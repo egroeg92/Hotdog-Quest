3cd1d6e0586a012d6fe51e6cb43bec8daac1475b
John Saigle <john.saigle@gmail.com>
2015-04-07 17:01:18 -0400

    Add bullet script and prefab

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Destroy bullets after 5 seconds
+		Destroy(this, 5.0f);
+		//rigidbody.AddForce(Vector3.forward * 10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
